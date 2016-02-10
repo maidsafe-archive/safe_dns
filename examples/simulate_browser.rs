e848420ae100d4eed5865a09e427eac1b4faa880
brian-js <brian.smith@maidsafe.net>
2016-02-10 14:50:06 +0000

    Build fix.

--- examples/simulate_browser.rs
+++ examples/simulate_browser.rs
@@ -35,13 +35,14 @@
 #![cfg_attr(feature="clippy", plugin(clippy))]
 #![cfg_attr(feature="clippy", deny(clippy, clippy_pedantic))]
 
+#![allow(unused_extern_crates)] #[macro_use]
+extern crate maidsafe_utilities;
 extern crate regex;
 extern crate routing;
 extern crate safe_dns;
 extern crate safe_core;
 extern crate safe_nfs;
 extern crate sodiumoxide;
-#[macro_use] extern crate maidsafe_utilities;
 
 use routing::Data;
 
